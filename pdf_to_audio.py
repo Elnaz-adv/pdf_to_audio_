2f718c1c4d85dee3d4e10f4fcb68c594160d0902
Elnaz-adv <elnaz.kh90@gmail.com>
2023-01-12 22:05:08 +0100

    comments are added

--- pdf_to_audio.py
+++ pdf_to_audio.py
@@ -1,11 +1,15 @@
 import PyPDF2,pyttsx3
 
-start_page = 140
+
+#inititialize
+start_page = 132
+end_page = 10
 
 my_pdf = PyPDF2.PdfReader(open('The-Invisible-Man.pdf','rb'))
 num_pages= len(my_pdf.pages)
 engine = pyttsx3.init()
 
+#Definning Functions you need
 def set_engine_properties(engine):
     engine.setProperty('rate', 130)    # Speed percent (can go over 100)
     engine.setProperty('volume', 0.9)  # Volume 0-1
@@ -25,11 +29,13 @@ set_engine_properties(engine)
 change_voice(engine, "Microsoft Zira Desktop - English (United States)")
 
 
+# Main Program
 for page_num in range(start_page,num_pages):
     text = my_pdf.pages[page_num].extract_text()
     clean_text = text.strip().replace('\n','')
     print(clean_text)
-
+    
+#Save as mp3 file    
 engine.save_to_file(clean_text,'invisible_man.mp3')
 engine.runAndWait()
 
